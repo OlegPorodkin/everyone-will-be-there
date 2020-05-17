9402864151b12054756f7ddfb873ec7a88785758
OlegPorodkin <p2o7r0o4d9k0in>
2020-05-17 21:48:12 +0300

    added flywaydb. Refactoring

--- src/main/java/ru/porodkin/everyonewillbethere/entity/BusStation.java
+++ src/main/java/ru/porodkin/everyonewillbethere/entity/BusStation.java
@@ -10,7 +10,7 @@ import java.util.Set;
 @Entity
 public class BusStation {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
--- src/main/java/ru/porodkin/everyonewillbethere/entity/City.java
+++ src/main/java/ru/porodkin/everyonewillbethere/entity/City.java
@@ -9,7 +9,7 @@ import java.util.Set;
 @Entity
 public class City {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     @OneToMany
